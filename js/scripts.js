0c400b9fc74943207ae625d88a6bca5386a76131
Zachary Matthewstearn <zammo911@gmail.com>
2016-08-12 11:46:41 -0700

    hook up test input element and form submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
+// Begin Back-End Logic
+
+// End Back-End Logic
+
+// Begin Front-End Logic
+function resetSelectElement() {
+  $("#input-track option").attr("selected", false);
+  $("#input-track option:first-child").attr("selected", true);
+}
+
 $("#btn-survey").click(function(){
   $("#survey").show();
 });
+
+$("#btn-clear").click(function(){
+  resetSelectElement();
+});
+
+$("#btn-submit").click(function(){
+  $("#survey-form").trigger("submit");
+});
+
+$("#survey-form").submit(function(event){
+  event.preventDefault();
+
+  console.log($("#input-track").val());
+
+  resetSelectElement();
+});
+// End Front-End Logic
